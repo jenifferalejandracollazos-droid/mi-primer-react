c9bae2d2a2bc6dec84a2ab333faf1c0368307167
Jeniffer Collazos <jecollazosdev@gmail.com>
2026-05-04 22:28:29 -0500

    feat: página ListaUsuarios con useEffect y fetch a API

---
+++ src/pages/ListaUsuarios.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+function ListaUsuarios () {
+    const [usuarios, setUsuarios] = useState([])
+    const [loading, setLoading] = useState (true)
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(data => {
+            setUsuarios(data)
+            setLoading(false)
+        })                                                                                                                                                                                               
+        }, [])
+
+        if (loading) {
+            return <p>Cargando...</p>
+        }
+
+        return (
+            <div>
+                <h1>Lista de Usuarios</h1>
+                <ul>
+                {usuarios.map(usuario => (
+                    <li key = {usuario.id}>{usuario.name} - {usuario.email}</li>
+                ))}
+                </ul>
+            </div>
+        )
+
+    
+        
+
+}
+
+export default ListaUsuarios;
